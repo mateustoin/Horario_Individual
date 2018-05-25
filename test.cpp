31e3b1944239ae0b3472572009ae1ae3c85da101
Mateus <mateustoin@gmail.com>
2018-05-25 20:37:08 -0300

    Xij implementado com sucesso

--- test.cpp
+++ test.cpp
@@ -13,10 +13,36 @@ int main(){
     float creditos[12] = {0};
     int preRequisito[12] = {0};
     int situacao[12] = {0};
-
+    
     string line;
-    ifstream arquivo("instancia.txt", ios::in);
+    
+    
+    string horario[12];
+    int choqueHorario[12][12]; //Armazena se tem choque de horario ou não
+    ifstream arquivo("choque.txt", ios::in);
  
+    for (int i = 0; i < 12; i++){
+        arquivo >> horario[i];
+    }
+
+    for (int i = 0; i < 12; i++){
+        for (int j = 0; j < 12; j++){
+            if (horario[i] == horario[j])
+                choqueHorario[i][j] = 1;
+            else
+                choqueHorario[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < 12; i++){
+        for (int j = 0; j < 12; j++){
+            cout << choqueHorario[i][j] << " ";
+        }
+        cout << endl;
+    }
+/*
+    ifstream arquivo("instancia.txt", ios::in);
+    
     //Armazenando identificador da disciplina
     for (int i = 0; i < 12; i++){
         arquivo >> identificador[i];
@@ -27,14 +53,32 @@ int main(){
         arquivo >> creditos[i];
     }
 
+    int preReqI[12][12];
     //Armazenando Pré-requisito da disciplina
     for (int i = 0; i < 12; i++){
-        arquivo >> creditos[i];
+        arquivo >> preRequisito[i];
+    }
+
+    for (int i = 0; i < 12; i++){
+        for (int j = 0; j < 12; j++){
+            if (preRequisito[i] == identificador[j])
+                preReqI[i][j] = preRequisito[i];
+            else
+                preReqI[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < 12; i++){
+        for (int j = 0; j < 12; j++){
+            cout << preReqI[i][j] << " ";
+        }
+        cout << endl;
     }
 
     //Armazenando se disciplina foi paga ou não
     for (int i = 0; i < 12; i++){
         arquivo >> creditos[i];
     }
-
+*/
+    return 0;
 }
