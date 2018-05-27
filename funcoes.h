dee87fa6c53368e56fe4b15d42652288e20d06dc
Mateus <mateustoin@gmail.com>
2018-05-27 20:57:12 -0300

    Codigo mais organizado

---
+++ funcoes.h
@@ -0,0 +1,33 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "UFFLP/UFFLP.h"
+
+#define LIMITE_CREDITOS 5
+#define MIN_PERIODOS 4
+#define CRED_ESTAGIO 5
+
+using namespace std;
+
+struct Data{
+    /*
+     *  Disciplinas referentes aos identificadores
+     */
+    int *identificador;
+    float *creditos;
+    int *preRequisito;
+    int **preReqI;
+    bool **choqueHorarioI;
+    int *situacao;
+    int disciplinas;
+    int numPeriodosFaltantes;
+    int numDisciplinasOp;
+};
+
+void atualizaDados(Data *data);
+void solveCoin(Data *data);
+
+#endif
